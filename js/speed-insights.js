9816920331990e459ab975cce4086ae2167d0906
shiro-se <yasirbantul@gmail.com>
2026-07-06 14:08:33 +0700

    remove speed insight

--- js/speed-insights.js
+++
@@ -1,6 +0,0 @@
-// Vercel Speed Insights initialization
-import { injectSpeedInsights } from '../node_modules/@vercel/speed-insights/dist/index.mjs';
-
-// Initialize Speed Insights
-// Debug is automatically enabled in development environments
-injectSpeedInsights();
